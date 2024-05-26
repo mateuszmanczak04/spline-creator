169d4a928217ae02985aff887da8f5dc71834966
Mateusz Mańczak <mateuszmanczak@icloud.com>
2024-05-26 18:43:59 +0200

    create drawPoint function

---
+++ src/utils/drawPoint.ts
@@ -0,0 +1,14 @@
+const drawPoint = (
+	ctx: CanvasRenderingContext2D,
+	canvasHeight: number,
+	x: number,
+	y: number,
+) => {
+	ctx.beginPath();
+	ctx.moveTo(x, canvasHeight - y);
+	ctx.arc(x, canvasHeight - y, 5, 0, Math.PI * 2);
+	ctx.fill();
+	return 0;
+};
+
+export default drawPoint;
