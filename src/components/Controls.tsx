1a1271239964df8cfd1955df4983389361300a7b
Mateusz Mańczak <mateuszmanczak@icloud.com>
2024-05-27 21:56:59 +0200

    describe todo list

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -13,6 +13,13 @@ const Controls: FC<ControlsProps> = ({ points, setPoints }) => {
 		setPoints(points);
 	};
 
+	// TODO
+	// display 2 inputs for each point, 1st for x position and 1nd for y
+	// control their changes
+	// remove "Update points" button and display changes immediately
+	// replace that button with "Download as image" button, add needed functionality
+	// create "Add new point" button which creates a new point with default {x:0, y:0}
+
 	return (
 		<div>
 			<h2>Współrzędne punktów</h2>
